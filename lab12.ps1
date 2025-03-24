7fbbb0d3a7f4b346f0e2b9690cf195a7016cb290
hiryamada <71864722+hiryamada@users.noreply.github.com>
2025-03-24 12:16:50 +0900

    Update and rename lab12.pwsh to lab12.ps1

--- lab12.ps1
+++ lab12.ps1
@@ -1,88 +1,66 @@
-################################
-# sign in to Azure using Azure CLI
-################################
-# specify username and password directly
-az login -u '' -p ''
-# or sign in interactively (web browser will open)
-# az login
+Function Step {
+	param([string]$Text)
+	Write-Output "#################################"
+	Write-Output "# $Text"
+	Write-Output "#################################"
+	Write-Host "Press Enter to continue:" -NoNewline
+	Read-Host
+}
 
-################################
-# download source codes
-################################
+Step 'Sign in to Azure using Azure CLI'
+az login
+
+Step 'Download source codes'
 $url = "https://github.com/hiryamada/mslearn-postgresql/archive/refs/heads/main.zip"
-$zipPath = 'main.zip'
+$currentTime = (Get-Date).ToString("HHmmss")
+$folder = "lab12-$currentTime"
+$zipPath = "$folder.zip"
 (New-Object System.Net.WebClient).DownloadFile($url, $zipPath)
 Expand-Archive -LiteralPath $zipPath
 
-################################
-# create resource group
-################################
+Step 'Create resource group'
 $REGION = "eastus"
 $RG_NAME = "rg-learn-postgresql-ai-$REGION"
 az group create --name $RG_NAME --location $REGION
 
-################################
-# generate password for postgres admin user
-################################
+Step 'Generate password for postgres admin user'
 $env:PGPASSWORD = -join ((48..57 + 65..90 + 97..122) | Get-Random -Count 18 | ForEach-Object {[char]$_})  
 
-################################
-# deploy Azure resources
-# (this will take approximately 5 minutes to complete)
-################################
-az deployment group create -g $RG_NAME --template-file .\main\mslearn-postgresql-main\Allfiles\Labs\Shared\deploy.bicep --parameters restore=false adminLogin=pgAdmin adminLoginPassword=$env:PGPASSWORD
+Step 'Deploy Azure resources. this will take approximately 5 minutes to complete.
+az deployment group create -g $RG_NAME --template-file .\$folder\mslearn-postgresql-main\Allfiles\Labs\Shared\deploy.bicep --parameters restore=false adminLogin=pgAdmin adminLoginPassword=$env:PGPASSWORD
 
-################################
-# define a function to send a SQL query using psql
-################################
+Step 'Define a function to send a SQL query using psql'
 $SERVER_FQDN = az postgres flexible-server list --query '[].fullyQualifiedDomainName' -o tsv
 Function ExecuteQuery { param([string]$Query) psql -U pgAdmin -h $SERVER_FQDN -d rentals -c $Query }
 
-################################
-# create tables
-################################
-
+Step 'Create tables'
 ExecuteQuery "CREATE TABLE listings (id int, name varchar(100), description text, property_type varchar(25), room_type varchar(30), price numeric, weekly_price numeric)"  
 ExecuteQuery "CREATE TABLE reviews (id int, listing_id int, date date, comments text)"  
 
-################################
-# copy data into tables
-################################
+Step 'Copy data into tables'
 ExecuteQuery "\COPY listings FROM 'main/mslearn-postgresql-main/Allfiles/Labs/Shared/listings.csv' CSV HEADER"
 ExecuteQuery "\COPY reviews FROM 'main/mslearn-postgresql-main/Allfiles/Labs/Shared/reviews.csv' CSV HEADER"
 
-################################
-# enable azure_ai extension
-################################
+Step 'Enable azure_ai extension'
 ExecuteQuery "CREATE EXTENSION IF NOT EXISTS azure_ai"
 
-################################
-# get Azure AI Services resource info
-################################
+Step 'Get Azure AI Services resource info'
 $AOAI_NAME = az cognitiveservices account list --query '[?kind==`AIServices`].name' -o tsv
 $LANG_ENDPOINT = (az cognitiveservices account show -n $AOAI_NAME -g $RG_NAME --query 'properties.endpoint' -o tsv)
 $AOAI_ENDPOINT = $LANG_ENDPOINT -replace 'cognitiveservices', 'openai'
 $AOAI_KEY = az cognitiveservices account keys list -n $AOAI_NAME -g $RG_NAME --query 'key1' -o tsv
 
-################################
-# set endpoint and key for Azure OpenAI
-################################
+Step 'Set endpoint and key for Azure OpenAI'
 ExecuteQuery "SELECT azure_ai.set_setting('azure_openai.endpoint', '$AOAI_ENDPOINT')"
 ExecuteQuery "SELECT azure_ai.set_setting('azure_openai.subscription_key', '$AOAI_KEY')"
 
-################################
-# create embeddings
-################################
+Step 'Create embeddings'
 ExecuteQuery "SELECT id, name, azure_openai.create_embeddings('embedding', description) AS vector FROM listings LIMIT 1"
 
-################################
-# set endpoint and key for Azure AI Language
-################################
+Step 'Set endpoint and key for Azure AI Language'
 ExecuteQuery "SELECT azure_ai.set_setting('azure_cognitive.endpoint', '$LANG_ENDPOINT')"
 ExecuteQuery "SELECT azure_ai.set_setting('azure_cognitive.subscription_key', '$AOAI_KEY')"
 
-################################
-# analyze sentiment
-################################
+Step 'Analyze sentiment'
 ExecuteQuery "SELECT id, comments, azure_cognitive.analyze_sentiment(comments, 'en') AS sentiment FROM reviews WHERE id in (1, 3)"
 
